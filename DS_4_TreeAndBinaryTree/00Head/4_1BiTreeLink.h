335098d63b102d256534327a3a7f9d1b794ba268
BILY5354 <2587547190@qq.com>
2022-11-09 21:56:03 +0800

    update tree

--- DS_4_TreeAndBinaryTree/00Head/4_1BiTreeLink.h
+++ DS_4_TreeAndBinaryTree/00Head/4_1BiTreeLink.h
@@ -1,3 +1,5 @@
+//链式存储的二叉树
+
 #ifndef BitTreeHead
 #define BitTreeHead
 #include <iostream>
@@ -6,9 +8,41 @@ using namespace std;
 
 #define MaxSize 50
 
-//定义结构
+//*STRAT定义模块
+struct ElemType
+{
+    int value;
+};
 
+typedef struct BitNode
+{
+    ElemType data;
+    struct BitNode *lchild, *rchild;
+} BitNode, *BiTree;
 
+//*END定义模块
+
+//*STRAT实现模块
+void InitTree(BiTree root)
+{
+    root = (BiTree)malloc(sizeof(BitNode));
+    root->data = {1};
+    root->lchild = nullptr;
+    root->rchild = nullptr;
+}
+
+//插入新节点
+bool InsertNode(BiTree T,ElemType val){
+    BitNode *p=(BitNode *)malloc(sizeof(BitNode));
+    p->data=val;
+    p->lchild=nullptr;
+    p->rchild=nullptr;
+    T->lchild=p;//作为左孩子
+}
+//*END实现模块
+
+//*STRAT模块
+//*END模块
 //函数声明/*
 
 #endif
